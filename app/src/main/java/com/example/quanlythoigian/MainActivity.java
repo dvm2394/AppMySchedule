36da7f8ce2ba8fdc1f6e40a4d0978eccfa90fca9
Mạnh <manh23092004@gmail.com>
2023-07-25 15:33:26 +0700

    heh

--- app/src/main/java/com/example/quanlythoigian/MainActivity.java
+++ app/src/main/java/com/example/quanlythoigian/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.quanlythoigian;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.example.quanlythoigian.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.txt.setText("he");
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
